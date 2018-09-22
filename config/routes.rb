a5d1f369689319035d071a2e46516192a99add58
原田悠佑 <adsoofmelk1327@gmail.com>
2018-09-22 16:58:49 +0900

    ルート作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
+  namespace :api, format: 'json' do
+    resource :tasks, only: [:index, :create, :update]
+  end
+
 end
