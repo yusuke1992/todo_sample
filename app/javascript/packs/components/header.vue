3034a35963a179470f3c2e96f7b315dcfc5fad41
原田悠佑 <adsoofmelk1327@gmail.com>
2018-09-22 17:52:25 +0900

    header_componentを作成

--- app/javascript/packs/components/header.vue
+++ app/javascript/packs/components/header.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
     <ul id="dropdown" class="dropdown-content">
-      <li><a href="#">Top</a></li>
-      <li><a href="#">About</a></li>
+      <li><a href="/">Top</a></li>
+      <li><a href="/about">About</a></li>
       <li><a href="#">Contact</a></li>
     </ul>
     <nav>
       <div class="nav-wrapper container">
         <a href="/" class="brand-logo left">Todo Application</a>
         <ul class="right hide-on-med-and-down">
-          <li><a href="#">Top</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="/">Top</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
         <ul class="right hide-on-large-only">
           <li>
