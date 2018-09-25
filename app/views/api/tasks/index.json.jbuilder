9fc96544fedb1ac351b5e2af0af9c78ca36709b6
原田悠佑 <adsoofmelk1327@gmail.com>
2018-09-25 18:07:00 +0900

    新規タスク作成ajax機能

--- app/views/api/tasks/index.json.jbuilder
+++ app/views/api/tasks/index.json.jbuilder
@@ -1,5 +1,10 @@
 json.set! :tasks do
   json.array! @tasks do |task|
     json.extract! task, :id, :name, :is_done, :created_at, :updated_at
+    # json.id task.id
+    # json.name task.name
+    # json.is_done task.is_done
+    # json.created_at task.created_at
+    # json.updated_at task.updated_at
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
   namespace :api, format: 'json' do
-    resource :tasks, only: [:index, :create, :update]
+    resources :tasks, only: [ :index, :create, :update ]
   end
   get '/about',   to: 'home#index'
   get '/contact',   to: 'home#index'
